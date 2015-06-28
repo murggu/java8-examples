5da4e338be051d3d6b33e50c8402434490632871
Aitor Murguzur <amurguzur@gmail.com>
2015-06-28 12:48:07 +0200

    Add Collector examples

---
+++ src/main/java/com/murggu/java8/samples/stream/Streams4.java
@@ -0,0 +1,104 @@
+package com.murggu.java8.samples.stream;
+
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+/**
+ * @author Aitor Murguzur
+ */
+public class Streams4 {
+
+    public static void main(String[] args) {
+
+        /**
+         * Collect terminal operation. Collect accepts a Collector which consists of four different operations: a
+         * supplier, an accumulator, a combiner, and a finisher.
+         */
+
+        List<Person> persons =
+                Arrays.asList(
+                        new Person("Max", 18),
+                        new Person("Peter", 23),
+                        new Person("Pamela", 23),
+                        new Person("David", 12));
+
+        List<Person> filtered =
+                persons
+                        .stream()
+                        .filter(p -> p.name.startsWith("P"))
+                        .collect(Collectors.toList());
+
+        System.out.println(filtered);
+
+        Map<Integer, List<Person>> personsByAge =
+                persons.stream()
+                    .collect(Collectors.groupingBy(p -> p.age));
+
+        personsByAge
+                .forEach((age, p) -> System.out.format("age %s: %s\n", age, p));
+
+        // aggregations using Collectors
+        Double averageAge = persons
+                .stream()
+                .collect(Collectors.averagingInt(p -> p.age));
+
+        System.out.println(averageAge);
+
+        // the summarizing Collector
+        IntSummaryStatistics ageSummary =
+                persons
+                        .stream()
+                        .collect(Collectors.summarizingInt(p -> p.age));
+
+        System.out.println(ageSummary);
+
+        // joining in a single string. Accepts a delimiter, optional prefix and suffix
+        String phrase = persons
+                .stream()
+                .filter(p -> p.age >= 18)
+                .map(p -> p.name)
+                .collect(Collectors.joining(" and ", "In Germany ", " are of legal age."));
+
+        System.out.println(phrase);
+
+        // to Map
+        Map<Integer, String> map = persons
+                .stream()
+                .collect(Collectors.toMap(
+                        p -> p.age,
+                        p -> p.name,
+                        (name1, name2) -> name1 + ";" + name2));
+
+        System.out.println(map);
+
+        // custom Collector
+        Collector<Person, StringJoiner, String> personNameCollector =
+                Collector.of(
+                        () -> new StringJoiner(" | "),          // supplier
+                        (j, p) -> j.add(p.name.toUpperCase()),  // accumulator
+                        (j1, j2) -> j1.merge(j2),               // combiner
+                        StringJoiner::toString);                // finisher
+
+        String names = persons
+                .stream()
+                .collect(personNameCollector);
+
+        System.out.println(names);
+    }
+
+   static  class Person {
+        String name;
+        int age;
+
+        Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+}
